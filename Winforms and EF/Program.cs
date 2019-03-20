3b4e76dcfb7d6f5e865dbbcafc16f9dbde0c7456
melifas <meletisfasoulis@yahoo.gr>
2019-03-20 13:18:47 +0200

    First change

--- Winforms and EF/Program.cs
+++ Winforms and EF/Program.cs
@@ -17,6 +17,7 @@ namespace Winforms_and_EF
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
